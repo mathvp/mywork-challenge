6fc6649907d25371c867f0d77d84a83e2928c426
Matheus Vieira <matheusvieiradepaula@gmail.com>
2019-07-02 21:45:50 -0300

    Disable Register Timetrack button when outside Geofences

--- app/controllers/timetracks_controller.rb
+++ app/controllers/timetracks_controller.rb
@@ -6,9 +6,14 @@ class TimetracksController < ApplicationController
   def new
     @timetrack = Timetrack.new(timetrack_params)
     @geofences = Geofence.check_timetrack_location(@timetrack)
+
+    error_msg = 'It doesn\'t have any Geofence near you...'
+    @timetrack.errors.add(:base, error_msg) if @geofences.size.zero?
   end
 
   def show
+    redirect_to location_timetracks_path unless params[:latitude] &&
+                                                params[:longitude]
     @timetrack = Timetrack.find_by(id: params[:id])
   end
 
